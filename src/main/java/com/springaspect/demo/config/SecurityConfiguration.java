c41dd02da4ba4722ed199340d35764fdbbc01f00
Wedson Quintanilha da Silva <wedson.silva@basis.com.br>
2021-05-06 17:16:07 -0300

    Add XSRF TOKEN

---
+++ src/main/java/com/springaspect/demo/config/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package com.springaspect.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter	{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse ());
+
+	}
+
+}
